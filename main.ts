d7a621d74f376b96d3bea58142c7b626cceba107
thomlund <t.lundgaard@outlook.dk>
2026-02-15 19:39:05 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,17 @@
 mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        3 3 . . . . . . . . . . . . . . 
-        . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, null, 50, 50)
+    projectile = sprites.createProjectileFromSprite(assets.image`skud`, null, 50, 50)
 })
 mp.onControllerEvent(ControllerEvent.Connected, function (player2) {
-    mySprite = mp.getPlayerBySprite(mp.getPlayerSprite(mySprite))
-    scene.cameraFollowSprite(mySprite)
+	
 })
-let mySprite: mp.Player = null
 let projectile: Sprite = null
+let mySprite: Sprite = null
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`player`, SpriteKind.Player))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`player`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`player`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`player`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`player`, SpriteKind.Player))
 tiles.setCurrentTilemap(tilemap`level1`)
 for (let value of mp.allPlayers()) {
     mp.moveWithButtons(value)
 }
+scene.cameraFollowSprite(mp.getPlayerSprite(mp.getPlayerBySprite(mySprite)))
